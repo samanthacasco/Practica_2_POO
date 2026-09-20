3be38cf0f5b654ed9e46690985f278013882250d
Samantha Casco <scascod@hotmail.com>
2026-09-19 19:58:20 -0600

    Adjunto clase principal

---
+++ PrincipalClinica.java
@@ -0,0 +1,86 @@
+public class PrincipalClinica { 
+    public static void main(String[] args) { 
+        Persona p1, p2, p3; 
+        Mascota m1, m2, m3;
+        
+        p1 = new Persona("Sara Salazar", "VET-001"); 
+        p2 = new Persona("Jose Murillo", "VET-002"); 
+        p3 = new Persona("Katherine Salas", "VET-003");
+        
+        m1 = new Mascota("Toby", "Labrador"); 
+        m2 = new Mascota("Max", "Pitbul", 2020); 
+        m3 = new Mascota("Nala", "Pastor Alemán", 2018);
+        
+        System.out.println("---- ESTADO INICIAL DE LAS PERSONAS ----");
+        
+        System.out.println(p1.toString()); 
+        System.out.println(p2.toString()); 
+        System.out.println(p3.toString()); 
+        
+        System.out.println(); 
+        
+        System.out.println("---- ESTADO INICIAL DE LAS MASCOTAS ----");
+        
+        System.out.println(m1.toString()); 
+        System.out.println(m2.toString()); 
+        System.out.println(m3.toString());
+        
+        System.out.println();
+        
+        m1.asignarVeterinario(p1); 
+        p1.asignarMascota(m1); 
+ 
+        m2.asignarVeterinario(p1); 
+        p1.asignarMascota(m2); 
+ 
+        m3.asignarVeterinario(p2); 
+        p2.asignarMascota(m3); 
+ 
+        System.out.println("---- PERSONA VETERINARIA DE CADA MASCOTA ----"); 
+ 
+        System.out.println( 
+                "Veterinario de " + m1.getNombre() + ": " 
+                + m1.consultaNombreVeterinario()); 
+ 
+        System.out.println( 
+                "Veterinario de " + m2.getNombre() + ": " 
+                + m2.consultaNombreVeterinario()); 
+ 
+        System.out.println( 
+                "Veterinario de " + m3.getNombre() + ": " 
+                + m3.consultaNombreVeterinario()); 
+ 
+        System.out.println(); 
+ 
+ 
+        System.out.println("---- MASCOTAS ASIGNADAS A CADA PERSONA ----"); 
+ 
+        System.out.println( 
+                p1.getNombre() + ": " 
+                + p1.consultarNombreMisMascotas()); 
+ 
+        System.out.println( 
+                p2.getNombre() + ": " 
+                + p2.consultarNombreMisMascotas()); 
+ 
+        System.out.println( 
+                p3.getNombre() + ": " 
+                + p3.consultarNombreMisMascotas()); 
+ 
+        System.out.println(); 
+ 
+        System.out.println("---- ESTADO FINAL DE LAS MASCOTAS ----"); 
+ 
+        System.out.println(m1.toString()); 
+        System.out.println(m2.toString()); 
+        System.out.println(m3.toString()); 
+ 
+        System.out.println(); 
+ 
+        System.out.println("---- ESTADO FINAL DE LAS PERSONAS ----"); 
+ 
+        System.out.println(p1.toString()); 
+        System.out.println(p2.toString()); 
+        System.out.println(p3.toString()); 
+    }
+}
